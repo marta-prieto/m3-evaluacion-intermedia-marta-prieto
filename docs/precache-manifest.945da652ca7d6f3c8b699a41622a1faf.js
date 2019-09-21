1c5bdd0f32c75ce15778363e171b1284db92b2c8
Jorge <martaprietoluque@gmail.com>
2019-09-21 13:03:40 +0200

    Add build docs

---
+++ docs/precache-manifest.945da652ca7d6f3c8b699a41622a1faf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b92b1ad7529b6869d9c4a9adb3fab62f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "171a72dda2b7b3638f67",
+    "url": "./static/css/main.37369f20.chunk.css"
+  },
+  {
+    "revision": "daf144cf9552bc9c7de9",
+    "url": "./static/js/2.a0074b3e.chunk.js"
+  },
+  {
+    "revision": "171a72dda2b7b3638f67",
+    "url": "./static/js/main.6c7ceb1a.chunk.js"
+  },
+  {
+    "revision": "e1c2b021dd4b726f3de1",
+    "url": "./static/js/runtime~main.020db0c3.js"
+  }
+]);
